c0683e924082735bba5cc86034e0cf58923739df
0xIfeoluwa <abisoyew1@gmail.com>
2023-11-25 18:14:46 +0100

    any commit message

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:kabar/home.dart';
 
+final lightTheme = ThemeData(
+  textSelectionTheme:
+      const TextSelectionThemeData(cursorColor: Color(0xff4E4B66)),
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(6),
+      borderSide: const BorderSide(
+        color: Color(0xff4E4B66),
+      ),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(6),
+      borderSide: const BorderSide(
+        color: Color(0xff4E4B66),
+      ),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(6),
+      borderSide: const BorderSide(
+        color: Color(0xff4E4B66),
+      ),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(6),
+      borderSide: const BorderSide(
+        color: Color(0xff4E4B66),
+      ),
+    ),
+    disabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(6),
+      borderSide: const BorderSide(
+        color: Colors.red,
+        width: 0.7,
+      ),
+    ),
+    focusedErrorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(6),
+      borderSide: const BorderSide(
+        color: Colors.red,
+      ),
+    ),
+  ),
+);
+
 void main() => runApp(
-      const MaterialApp(
-        home: Kadar(),
+      MaterialApp(
+        home: const Kadar(),
+        theme: lightTheme,
       ),
     );
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
+import 'package:form_validator/form_validator.dart';
 // import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kabar/screens/forget_password.dart';
@@ -12,269 +15,273 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  late GlobalKey<FormState> _formKey;
+  late TextEditingController _emailController, _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _formKey = GlobalKey<FormState>();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _formKey.currentState?.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void login() {
+    if (_formKey.currentState!.validate()) {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 20.0, left: 20, right: 20),
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            SizedBox(
-              width: 379,
-              height: 240,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hello",
-                    style: GoogleFonts.poppins(
-                        fontSize: 48, fontWeight: FontWeight.w700),
-                  ),
-                  Text(
-                    "Again!",
-                    style: GoogleFonts.poppins(
-                      fontSize: 48,
-                      fontWeight: FontWeight.w700,
-                      color: const Color.fromRGBO(24, 119, 242, 1),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 222,
-                    height: 60,
-                    child: Text(
-                      "Welcome back you've been missed",
-                      style: GoogleFonts.poppins(
-                          fontSize: 20, fontWeight: FontWeight.w400),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20.0, left: 20, right: 20),
+          child: Form(
+            key: _formKey,
+            child: Column(
               children: [
-                Text(
-                  "Username",
-                  style: GoogleFonts.poppins(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                const Text(
-                  "*",
-                  style: TextStyle(
-                    color: Colors.red,
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                border: Border.all(
-                  color: Colors.grey,
-                  style: BorderStyle.solid,
-                ),
-                borderRadius: BorderRadius.circular(6.0),
-              ),
-              child: const Column(
-                children: [
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      prefixStyle: TextStyle(
-                        color: Color.fromRGBO(24, 119, 242, 1),
-                        fontWeight: FontWeight.bold,
+                SizedBox(
+                  width: 379,
+                  height: 240,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Hello",
+                        style: GoogleFonts.poppins(
+                            fontSize: 48, fontWeight: FontWeight.w700),
                       ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Row(
-                children: [
-                  Text(
-                    "Password",
-                    style: GoogleFonts.poppins(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  const Text(
-                    "*",
-                    style: TextStyle(
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                border: Border.all(
-                  color: Colors.grey,
-                  style: BorderStyle.solid,
-                ),
-                borderRadius: BorderRadius.circular(6.0),
-              ),
-              child: const Column(
-                children: [
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      prefixStyle: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
+                      Text(
+                        "Again!",
+                        style: GoogleFonts.poppins(
+                          fontSize: 48,
+                          fontWeight: FontWeight.w700,
+                          color: const Color.fromRGBO(24, 119, 242, 1),
+                        ),
                       ),
-                    ),
+                      SizedBox(
+                        width: 222,
+                        height: 60,
+                        child: Text(
+                          "Welcome back you've been missed",
+                          style: GoogleFonts.poppins(
+                              fontSize: 20, fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
+                ),
                 Row(
                   children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.check_box),
-                      color: const Color.fromRGBO(24, 119, 242, 1),
-                    ),
                     Text(
-                      "Remeber me",
-                      style: GoogleFonts.poppins(
-                          fontSize: 14, fontWeight: FontWeight.w400),
-                    ),
-                  ],
-                ),
-                TextButton(
-                  onPressed: () => {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ForgetPassword())),
-                  },
-                  child: Text(
-                    "Forget the Password?",
-                    style: GoogleFonts.poppins(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      letterSpacing: 0.12,
-                      color: const Color.fromRGBO(24, 119, 242, 1),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              color: const Color.fromRGBO(24, 119, 242, 1),
-              child: SizedBox(
-                width: 379,
-                height: 50,
-                child: OutlinedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Login",
-                    style: GoogleFonts.poppins(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 0.12,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(18.0),
-              child: Center(
-                child: Text("or continue with"),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  width: 165,
-                  height: 48,
-                  child: Container(
-                    color: const Color.fromRGBO(238, 241, 244, 1),
-                    child: OutlinedButton(
-                      onPressed: () {},
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.facebook),
-                          Text(
-                            "facebook",
-                            style: TextStyle(
-                              color: Color.fromRGBO(102, 112, 128, 1),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 165,
-                  height: 48,
-                  child: Container(
-                    color: const Color.fromRGBO(238, 241, 244, 1),
-                    child: OutlinedButton(
-                      onPressed: () {},
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image(
-                            width: 24,
-                            height: 24,
-                            image: AssetImage("images/google.png"),
-                          ),
-                          Text(
-                            "Google",
-                            style: TextStyle(
-                              color: Color.fromRGBO(102, 112, 128, 1),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(6.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("don't have an account?"),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const SignUp()));
-                    },
-                    child: Text(
-                      "sign Up",
+                      "Username",
                       style: GoogleFonts.poppins(
                         fontSize: 14,
                         fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const Text(
+                      "*",
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  validator: ValidationBuilder().required().build(),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  keyboardType: TextInputType.name,
+                  decoration: const InputDecoration(
+                    prefixStyle: TextStyle(
+                      color: Color.fromRGBO(24, 119, 242, 1),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Password",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      const Text(
+                        "*",
+                        style: TextStyle(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  validator: ValidationBuilder().required().build(),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  keyboardType: TextInputType.name,
+                  decoration: const InputDecoration(
+                    prefixStyle: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.check_box),
+                          color: const Color.fromRGBO(24, 119, 242, 1),
+                        ),
+                        Text(
+                          "Remeber me",
+                          style: GoogleFonts.poppins(
+                              fontSize: 14, fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                    TextButton(
+                      onPressed: () => {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ForgetPassword())),
+                      },
+                      child: Text(
+                        "Forget the Password?",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0.12,
+                          color: const Color.fromRGBO(24, 119, 242, 1),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 379,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: login,
+                    child: Text(
+                      "Login",
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
                         letterSpacing: 0.12,
-                        color: const Color.fromRGBO(24, 119, 242, 1),
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                ],
-              ),
-            )
-          ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(18.0),
+                  child: Center(
+                    child: Text("or continue with"),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 165,
+                      height: 48,
+                      child: Container(
+                        color: const Color.fromRGBO(238, 241, 244, 1),
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.facebook),
+                              Text(
+                                "facebook",
+                                style: TextStyle(
+                                  color: Color.fromRGBO(102, 112, 128, 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 165,
+                      height: 48,
+                      child: Container(
+                        color: const Color.fromRGBO(238, 241, 244, 1),
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image(
+                                width: 24,
+                                height: 24,
+                                image: AssetImage("images/google.png"),
+                              ),
+                              Text(
+                                "Google",
+                                style: TextStyle(
+                                  color: Color.fromRGBO(102, 112, 128, 1),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("don't have an account?"),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SignUp(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          "sign Up",
+                          style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 0.12,
+                            color: const Color.fromRGBO(24, 119, 242, 1),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
